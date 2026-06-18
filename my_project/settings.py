50010866a4c4faf13a71303ae099664b7e766878
artem-builds <thetkachenkoartem@gmail.com>
2026-06-18 12:24:03 +0100

    add settings for deployement the project to herocu

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zhte#-di7vwjr(3gu!5d5)@&j=u$8!_2p%91_s7+*ce#$4n*i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
